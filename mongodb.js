8ade60b2820a8defb4c8f198ef526d6031eab65a
Mu Wei Kuo <kmw03062@hotmail.com>
2020-11-18 13:57:06 +1000

    playing with the crud operations

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,15 @@
-//CRUD create read update delete
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// CRUD create read update delete
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
 
 const connectionURL = 'mongodb://localhost:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id.id);
+console.log(id.toHexString());
 
 //async database connection  operation
 MongoClient.connect(connectionURL, {
@@ -14,8 +20,28 @@ MongoClient.connect(connectionURL, {
         return console.log('unable to connect to database!');
     }
     const db = client.db(databaseName);
-    // db.collection('users').insertOne({
-    //     name: 'Jim',
-    //     age: 29
+    // db.collection('users').findOne({
+    //     _id: new ObjectID("5fb36664ad310432c461e5cb")
+    // }, (err, res) => {
+    //     if (err) {
+    //         return console.log('unable to fetch user');
+    //     }
+    //     console.log(res);
     // });
+    db.collection('users').find({
+        age: 25
+    }).toArray((err, res) => {
+        if (err) {
+            return console.log('unable to fetch user');
+        }
+        console.log(res);
+    });
+    db.collection('users').find({
+        age: 25
+    }).count((err, res) => {
+        if (err) {
+            return console.log('unable to fetch user');
+        }
+        console.log(res);
+    });
 });

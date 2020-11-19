fa282862a5beed8f2876fdee86a70671fae93254
Mu Wei Kuo <kmw03062@hotmail.com>
2020-11-19 17:24:02 +1000

    installed validator module and practice using it in the mongoose models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator').default;
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -8,20 +9,70 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(val) {
+            if (!validator.isEmail(val)) {
+                throw new Error('Not a valid email address');
+            }
+        },
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(val) {
+            if (val < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        validate(val) {
+            if (val.toLowerCase().includes('password')) {
+                throw new Error('invalid password as it include the password word!');
+            }
+        },
+        trim: true
     }
 });
 
-const me = new User({
-    name: "Jim",
-    age: '2rr9'
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
-me.save().then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log('Error!', err);
+const newTask = new Task({
+    description: 'Learn MongoDB APIs',
+    completed: false
 });
+
+newTask.save();
+
+// const me = new User({
+//     name: '  Kevin  ',
+//     email: 'kevin123@GMail.com',
+//     password: '    mynEEPASSWORDsamp     '
+// });
+
+// me.save().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log('Error!', err);
+// });
